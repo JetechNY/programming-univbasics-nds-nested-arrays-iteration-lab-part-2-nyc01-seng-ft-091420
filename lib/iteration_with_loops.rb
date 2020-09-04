aa39f335ba184d5eb6617535dc47985df3d5d1ae
SunJet Liu <sunjetliu@gmail.com>
2020-09-04 16:51:59 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -3,12 +3,14 @@ def find_min_in_nested_arrays(src)
   row_index = 0
   while row_index < src.count do
     element_index = 0
+    element_index1 = 1
     arr = []
     while element_index < src[row_index].count do
-       if src[row_index][element_index] < src[row_index][element_index + 1]
+       if src[row_index][element_index] < src[row_index][element_index1]
         arr = src[row_index][element_index]
       end
       element_index += 1
+      element_index1 += 1
     end
     outer_results << arr
     row_index += 1
